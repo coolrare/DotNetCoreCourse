5ce624bcf78d163f608575e83a4c534b876da7f8
Vulcan <vulcan.lee@gmail.com>
2018-01-21 10:18:42 +0800

    Lab 修正

--- NETPackage/NETSandardLib/NETSandardLib/Class1.cs
+++ NETPackage/NETSandardLib/NETSandardLib/Class1.cs
@@ -4,5 +4,17 @@ namespace NETSandardLib
 {
     public class Class1
     {
+        public void Show()
+        {
+            Console.WriteLine("Hello .NET Standard");
+            Console.WriteLine("List<string> 使用組件的資訊");
+            Console.WriteLine(
+                   typeof(System.Collections.Generic.List<string>).AssemblyQualifiedName);
+            Console.WriteLine();
+            Console.WriteLine("List<string> 使用組件所在路徑");
+            Console.WriteLine(
+                   typeof(System.Collections.Generic.List<string>).Assembly.Location);
+            Console.ReadKey();
+        }
     }
 }
